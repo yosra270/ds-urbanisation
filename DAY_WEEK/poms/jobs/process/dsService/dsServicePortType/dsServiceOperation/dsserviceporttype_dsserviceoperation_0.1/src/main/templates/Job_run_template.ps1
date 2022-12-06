91683afcae7f18206e31520b49db3c86372dd265
Yosra DRIDI <yosradridi72@gmail.com>
2022-12-06 18:04:16 +0100

    alll

---
+++ DAY_WEEK/poms/jobs/process/dsService/dsServicePortType/dsServiceOperation/dsserviceporttype_dsserviceoperation_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp 'null' day_week.dsserviceporttype_dsserviceoperation_0_1.dsServicePortType_dsServiceOperation $args
